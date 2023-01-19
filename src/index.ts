6d5640f3b056002d616a1d1bc47bf44389f614f4
Mauro E. Bender <mauro@amalgama.co>
2023-01-19 12:38:41 +0100

    Export package types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 export { default as Api } from './Api/Api';
 export { default as AuthApi } from './AuthApi/AuthApi';
+
 export {
-	FailedResponseError, InvalidCredentialsError, InvalidTokenRequestError, NetworkError
+	FailedResponseError,
+	InvalidCredentialsError,
+	InvalidTokenRequestError,
+	NetworkError
 } from './errors';
+
 export {
 	HTTP_OK,
 	HTTP_BAD_REQUEST,
@@ -11,3 +16,23 @@ export {
 	HTTP_NOT_FOUND,
 	HTTP_UNPROCESSABLE_ENTITY
 } from './constants';
+
+export type {
+	GrantType,
+	AuthApiParams,
+	Credentials,
+	AccessToken,
+	AccessTokenParams,
+	AccessTokenRefreshParams,
+	AccessTokenRequestResponse,
+	IAuthApi,
+	AccessTokenGetter,
+	AccessTokenUpdateCallback,
+	ApiParams,
+	HTTPMethod,
+	QueryParam,
+	QueryParams,
+	JSONValue,
+	RequestData,
+	RequestConfig
+} from './types';
